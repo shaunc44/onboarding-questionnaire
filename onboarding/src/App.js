ca8c96f76a47fa46303074114ebf16dada279102
Shaun <seyyes2000@hotmail.com>
2018-10-02 16:37:30 -0400

    work on setting up react components

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import ollie from './ollie.png';
 import './App.css';
 
 class App extends Component {
@@ -7,15 +8,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={ollie} className="App-logo" alt="logo" />
+          <p className="App-intro">
+            50% OFF your meal plan will be applied at checkout
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+// <h1 className="App-title">Ollie</h1>
+// <p className="App-intro">
+//  To get started, edit <code>src/App.js</code> and save to reload.
+// </p>
