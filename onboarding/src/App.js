0f0c63e1d9472a231823298b61139205e2af9d7c
Shaun <seyyes2000@hotmail.com>
2018-10-04 11:33:26 -0400

    work in input styling; need to finish app

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -18,6 +18,8 @@ class App extends Component {
       breed: '',
       sex: '',
       neuterSpay: '',
+      month: '',
+      year: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -46,27 +48,27 @@ class App extends Component {
         </header>
 
         <div class="onboarding-form-fields">
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">My name is </label>
-            <input type="text" name="ownerName" onChange={this.handleChange} />
+            <input class="onboarding-input" type="text" name="ownerName" onChange={this.handleChange} />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">and my pup's name is </label>
-            <input type="text" name="petName" onChange={this.handleChange} />
+            <input class="onboarding-input" type="text" name="petName" onChange={this.handleChange} />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">My zip code is </label>
-            <input type="number" name="zipCode" onChange={this.handleChange} />
+            <input class="onboarding-input" type="number" name="zipCode" onChange={this.handleChange} />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">My email is </label>
-            <input type="email" name="email" onChange={this.handleChange} />
+            <input class="onboarding-input" type="email" name="email" onChange={this.handleChange} />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">{this.state.petName} is a </label>
             <Autocomplete 
               value={this.state.value}
@@ -80,78 +82,72 @@ class App extends Component {
             />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">of a </label>
-            <select value={this.state.value} name="breed" onChange={this.handleChange}>
-              <option value="lab">labrador retriever</option>
-              <option value="poodle">poodle</option>
-              <option value="boxer">boxer</option>
-              <option value="chihuahua">chihuahua</option>
-              <option value="dachshund">dachshund</option>
-            </select>
+            <Autocomplete 
+              value={this.state.value}
+              name="breed"
+              onChange={this.handleChange}
+              suggestions={[
+                "lab",
+                "poodle",
+                "boxer",
+                "chihuahua",
+                "dachshund",
+              ]}
+            />
             <label class="onboarding-form-label"> to be precise.</label>
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">{this.state.petName} is a </label>
-            <select value={this.state.value} name="sex" onChange={this.handleChange}>
-              <option value="he">He</option>
-              <option value="she">She</option>
-            </select>
+            <Autocomplete 
+              value={this.state.value}
+              name="sex"
+              onChange={this.handleChange}
+              suggestions={[
+                "He",
+                "She",
+              ]}
+            />
             <label class="onboarding-form-label"> and is </label>
-            <select value={this.state.value} name="neuterSpay" onChange={this.handleChange}>
-              <option value="neutered/spayed">neutered/spayed</option>
-              <option value="not neutered/spayed">not neutered/spayed</option>
-            </select>
+            <Autocomplete 
+              value={this.state.value}
+              name="neuterSpay"
+              onChange={this.handleChange}
+              suggestions={[
+                "neutered/spayed",
+                "not neutered/spayed",
+              ]}
+            />
           </form>
 
-          <form>
+          <form class="onboarding-form-block">
             <label class="onboarding-form-label">{this.state.petName} was born in </label>
-            <select value={this.state.value} name="month" onChange={this.handleChange}>
-              <option value="Jan">Jan</option>
-              <option value="Feb">Feb</option>
-              <option value="Mar">Mar</option>
-              <option value="Apr">Apr</option>
-              <option value="May">May</option>
-              <option value="Jun">Jun</option>
-              <option value="Jul">Jul</option>
-              <option value="Aug">Aug</option>
-              <option value="Sep">Sep</option>
-              <option value="Oct">Oct</option>
-              <option value="Nov">Nov</option>
-              <option value="Dec">Dec</option>
-            </select>
+            <Autocomplete 
+              value={this.state.value}
+              name="month"
+              onChange={this.handleChange}
+              suggestions={[
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+              ]}
+            />
             <label class="onboarding-form-label">, </label>
-            <select value={this.state.value} name="year" onChange={this.handleChange}>
-              <option value="2018">2018</option>
-              <option value="2017">2017</option>
-              <option value="2016">2016</option>
-              <option value="2015">2015</option>
-              <option value="2014">2014</option>
-              <option value="2013">2013</option>
-              <option value="2012">2012</option>
-              <option value="2011">2011</option>
-              <option value="2010">2010</option>
-              <option value="2009">2009</option>
-              <option value="2008">2008</option>
-              <option value="2007">2007</option>
-              <option value="2006">2006</option>
-              <option value="2005">2005</option>
-              <option value="2004">2004</option>
-              <option value="2003">2003</option>
-              <option value="2002">2002</option>
-              <option value="2001">2001</option>
-              <option value="2000">2000</option>
-              <option value="1999">1999</option>
-              <option value="1998">1998</option>
-              <option value="1997">1997</option>
-              <option value="1996">1996</option>
-              <option value="1995">1995</option>
-              <option value="1994">1994</option>
-              <option value="1993">1993</option>
-              <option value="1992">1992</option>
-              <option value="1991">1991</option>
-            </select>
+            <Autocomplete 
+              value={this.state.value}
+              name="year"
+              onChange={this.handleChange}
+              suggestions={[
+                "2018", "2017", "2016", "2015", 
+                "2014", "2013", "2012", "2011", 
+                "2010", "2009", "2008", "2007", 
+                "2006", "2005", "2004", "2003", 
+                "2002", "2001", "2000", "1999", 
+                "1998", "1997", "1996", "1995", 
+                "1994", "1993", "1992", "1991",
+              ]}
+            />
           </form>
         </div>
       </div>
@@ -160,6 +156,15 @@ class App extends Component {
 }
 
 export default App;
+
+
+// <select class="onboarding-input" value={this.state.value} name="breed" onChange={this.handleChange}>
+              // <option value="lab">labrador retriever</option>
+              // <option value="poodle">poodle</option>
+              // <option value="boxer">boxer</option>
+              // <option value="chihuahua">chihuahua</option>
+              // <option value="dachshund">dachshund</option>
+            // </select>
 
 
 //<select value={this.state.value} name="mix" onChange={this.handleChange}>
